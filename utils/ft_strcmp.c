cb97d232764065378309f2e8cb49c73230758fea
Josue Samba Dianzungu <josuesamba99@gmail.com>
2026-08-17 19:44:49 +0200

    ft_strcmp for edf or fifo in arguments

---
+++ utils/ft_strcmp.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josamba- <josamba-@student.42belgium.com>  #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-04-07 15:20:30 by josamba-          #+#    #+#             */
+/*   Updated: 2026-04-07 15:20:30 by josamba-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+//#include "codexion.h"
+#include <stddef.h>
+
+int	ft_strncmp(const char *s1, const char *s2)
+{
+    size_t    i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+    i = 0;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (str1[i] && str2[i] && str1[i] == str2[i])
+		i++;
+	return ((int)(str1[i] - str2[i]));
+}
+/*#include <stdio.h>
+int main()
+{
+    char * str1 = "mama";
+    char * str2 = "maman";
+    printf("%d", ft_strncmp(str1, str2));
+}*/
