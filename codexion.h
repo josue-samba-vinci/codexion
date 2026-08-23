633c56b1187a90e1b834f40960173323b2aa00a3
Josue Samba <josuesamba99@gmail.com>
2026-08-23 12:13:42 +0200

    update codexion.h t_dongle and t_coder

--- codexion.h
+++ codexion.h
@@ -5,17 +5,37 @@
 # include <limits.h>
 # include <stddef.h>
 # include <stdio.h>
+# include <pthread.h> //mutex: init destroy lock unlock
+					  //threads: create join detach
+# include <sys/time.h> //gettimeofday
+
+typedef struct s_coder
+{
+	int	coder_id;
+	int	nb_compiles;
+	t_dongle	left_dongle;
+	t_dongle	right_dongle;
+	pthread_t	thread_id;
+}   t_coder;
+
+typedef struct s_dongle
+{
+	pthread_mutex_t	dongle;
+	int	dongle_id;
+}	t_dongle;
 
 typedef struct s_config
 {
-    int     nb_coders;
-    long    time_to_burnout;
-    long    time_to_compile;
-    long    time_to_debug;
-    long    time_to_refactor;
-    int     nb_compiles_required;
-    long    dongle_cooldown;
-    int     scheduler;
+	long    nb_coders;
+	long    time_to_burnout;
+	long    time_to_compile;
+	long    time_to_debug;
+	long    time_to_refactor;
+	int     nb_compiles_required;
+	long    dongle_cooldown;
+	int     scheduler;
+	long	start;
+	int		end; //evryone has reached the max number of compile or burnout (0/1)
 }   t_config;
 
 int	ft_atoi(const char *str);
