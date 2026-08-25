376a89fc7c523a669991e5398651040d78b1c8d7
Josue Samba <josuesamba99@gmail.com>
2026-08-25 18:27:54 +0200

    better strucure

--- codexion.h
+++ codexion.h
@@ -5,24 +5,45 @@
 # include <limits.h>
 # include <stddef.h>
 # include <stdio.h>
-# include <pthread.h> //mutex: init destroy lock unlock
-					  //threads: create join detach
+# include <unistd.h>  //sleep write
+# include <pthread.h> //mutex: init destroy lock unlock  threads: create join detach
 # include <sys/time.h> //gettimeofday
 
-typedef struct s_coder
+typedef struct s_config t_config;
+
+typedef struct s_request
 {
-	int	coder_id;
-	int	nb_compiles;
-	t_dongle	left_dongle;
-	t_dongle	right_dongle;
-	pthread_t	thread_id;
-}   t_coder;
+	int		coder_id;
+	long	key;
+}	t_request;
+
+typedef struct s_heap
+{
+	t_request	*data;
+	int			size;
+	int			capacity;
+}	t_heap;
 
 typedef struct s_dongle
 {
-	pthread_mutex_t	dongle;
-	int	dongle_id;
-}	t_dongle;
+    pthread_mutex_t  mutex;
+    pthread_cond_t   cond;
+    int              id;
+    int              available;
+    long             free_at;
+    t_heap           waiters;
+}   t_dongle;
+
+
+typedef struct s_coder
+{
+	int	id;
+	int	compile_count;
+	t_dongle	*left_dongle;
+	t_dongle	*right_dongle;
+	pthread_t	thread_id;
+	t_config	*table;
+}   t_coder;
 
 typedef struct s_config
 {
@@ -35,10 +56,13 @@ typedef struct s_config
 	long    dongle_cooldown;
 	int     scheduler;
 	long	start;
-	int		end; //evryone has reached the max number of compile or burnout (0/1)
+	int		end;
+	t_coder	*coders;
+	t_dongle	*dongles;
 }   t_config;
 
 int	ft_atoi(const char *str);
 int	ft_strcmp(const char *s1, const char *s2);
+int	parser(int argc, char **argv, t_config *tab);
 
 #endif
