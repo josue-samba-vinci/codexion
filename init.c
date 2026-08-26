c83d2ef9efb3fcafd61799b45722205f4da80145
Josue Samba <josuesamba99@gmail.com>
2026-08-26 15:08:13 +0200

    norm : init.c

--- init.c
+++ init.c
@@ -2,8 +2,8 @@
 
 int	init_dongles(t_config *config)
 {
-	t_dongle *dongle;
-	int	i;
+	t_dongle	*dongle;
+	int			i;
 
 	config->dongles = malloc(sizeof(t_dongle) * config->nb_coders);
 	if (!config->dongles)
@@ -26,29 +26,29 @@ int	init_dongles(t_config *config)
 	return (1);
 }
 
-
-void	assign_dongle(t_coder *coder, t_dongle *dongles, int coder_pos, int nb_coders)
+void	assign_dongle(
+	t_coder *coder, t_dongle *dongles, int coder_pos, int nb_coders
+)
 {
-	int id_first_dongle;
-	int id_second_dongle;
+	int	id_first_dongle;
+	int	id_second_dongle;
 
 	id_first_dongle = dongles[(coder_pos) % nb_coders].id;
-	id_second_dongle = dongles[(coder_pos+1) % nb_coders].id;
+	id_second_dongle = dongles[(coder_pos + 1) % nb_coders].id;
 	if (id_first_dongle > id_second_dongle)
 	{
-    	coder->second_dongle = &dongles[(coder_pos) % nb_coders];
-    	coder->first_dongle = &dongles[(coder_pos+1) % nb_coders];
+		coder->second_dongle = &dongles[(coder_pos) % nb_coders];
+		coder->first_dongle = &dongles[(coder_pos + 1) % nb_coders];
 		return ;
 	}
 	coder->first_dongle = &dongles[(coder_pos) % nb_coders];
-    coder->second_dongle = &dongles[(coder_pos+1) % nb_coders];
+	coder->second_dongle = &dongles[(coder_pos + 1) % nb_coders];
 }
-
 
 int	init_coders(t_config *config)
 {
-	int	i;
-	t_coder *coder;
+	int		i;
+	t_coder	*coder;
 
 	config->coders = malloc(sizeof(t_coder) * config->nb_coders);
 	if (!config->coders)
@@ -59,16 +59,16 @@ int	init_coders(t_config *config)
 		coder = config->coders + i;
 		pthread_mutex_init(&coder->mutex, NULL);
 		assign_dongle(coder, config->dongles, i, config->nb_coders);
-		coder->id = i+1;
-        coder->compile_count = 0;
-        coder->config = config;
+		coder->id = i + 1;
+		coder->compile_count = 0;
+		coder->config = config;
 		coder->last_compile = 0;
 		i++;
 	}
 	return (1);
 }
 
-int init_config(t_config *config)
+int	init_config(t_config *config)
 {
 	pthread_mutex_init(&config->print_lock, NULL);
 	pthread_mutex_init(&config->state_lock, NULL);
@@ -77,5 +77,5 @@ int init_config(t_config *config)
 		return (0);
 	if (!init_coders(config))
 		return (0);
-	return(1);
+	return (1);
 }
