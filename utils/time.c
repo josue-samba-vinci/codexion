4e9f045279d9942f248c745a171f5f7fa6407aff
Josue Samba <josuesamba99@gmail.com>
2026-08-26 15:33:11 +0200

    norm : time.c

--- utils/time.c
+++ utils/time.c
@@ -1,32 +1,22 @@
 #include "codexion.h"
 
-long    get_time_ms(void)
+long	get_time_ms(void)
 {
-    struct  timeval time;
-    
-    gettimeofday(&time, NULL);
-    return ((time.tv_sec*1000)+(time.tv_usec/1000));
+	struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
 }
 
-int sim_is_over(t_config *config)
+void	precise_sleep(long duration_ms, t_config *config)
 {
-    int actual_end;
+	long	start;
 
-    pthread_mutex_lock(&config->state_lock);
-    actual_end = config->end;
-    pthread_mutex_unlock(&config->state_lock);
-    return (actual_end);
-}
-
-void    precise_sleep(long duration_ms, t_config *config)
-{
-    long    start;
-
-    start = get_time_ms();
-    while (get_time_ms() - start < duration_ms)
-    {
-        if (sim_is_over(config))
-            return;
-        usleep(100);   
-    }
+	start = get_time_ms();
+	while (get_time_ms() - start < duration_ms)
+	{
+		if (sim_is_over(config))
+			return ;
+		usleep(100);
+	}
 }
