b69155fc95b0ccda2adc7734fe711d26a471d900
Josue Samba <josuesamba99@gmail.com>
2026-08-26 15:23:02 +0200

    norm : main.c

--- main.c
+++ main.c
@@ -1,17 +1,18 @@
 #include "codexion.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_config config;
+	t_config	config;
 
-    if (!parser(argc, argv, &config))
-        return(1);
-    printf("number of coders : %d\n", config.nb_coders);
+	if (!parser(argc, argv, &config))
+		return (1);
+	printf("number of coders : %d\n", config.nb_coders);
 	printf("time to burnout : %ld\n", config.time_to_burnout);
 	printf("time to compile : %ld\n", config.time_to_compile);
 	printf("time to debug : %ld\n", config.time_to_debug);
 	printf("time to refactor : %ld\n", config.time_to_refactor);
-	printf("number of compilation required : %d\n", config.nb_compiles_required);
+	printf("number of compilation required : %d\n",
+		config.nb_compiles_required);
 	printf("dongle cooldown time : %ld\n", config.dongle_cooldown);
 	printf("scheduler type : %d\n", config.scheduler);
 	if (!init_config(&config))
