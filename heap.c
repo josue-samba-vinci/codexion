df1f655458e24f2b526fd09bc2d2f52ce1ba35d3
Josue Samba Dianzungu <josuesamba99@gmail.com>
2026-08-31 15:57:58 +0200

    add: heap.c

--- heap.c
+++ heap.c
@@ -15,6 +15,21 @@
 t_request   *heap_top(t_heap *heap)
 {
     if (heap->size == 0)
-        return 0;
-    return heap->data[0];
+        return NULL;
+    return &heap->data[0];
 }
+
+void    heap_pop(t_heap *heap)
+{
+    heap->data[0] = heap->data[-1];
+    size--;
+    shift_down(heap, 0);
+}
+
+//void    shift_down(t_heap *heap, int index)
+//{
+//    int smallest;
+//
+//    smallest = 
+//}
+    
