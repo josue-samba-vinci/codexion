0bc19e884613ce980bd70e604c7d732751512a2c
Josue Samba Dianzungu <josuesamba99@gmail.com>
2026-09-01 13:28:30 +0200

    pop, push, top for my heap implementation

--- heap.c
+++ heap.c
@@ -12,28 +12,50 @@
 
 #include "codexion.h"
 
-t_request   *heap_top(t_heap *heap)
+t_request	*heap_top(t_heap *heap)
 {
-    if (heap->size == 0)
-        return NULL;
-    return &heap->data[0];
+	if (heap->size == 0)
+		return (NULL);
+	return (&heap->data[0]);
 }
 
-void    shift_down(t_heap *heap, int index)
+void	shift_down(t_heap *heap, int index)
 {
-    int min;
+	int	min;
 
-    min = smallest_child(heap, index);
-    if (min = index)
-        return ;
-    swap_request(&heap->data[index], &heap->data[min]);
-    shift_down(heap, min);
+	min = smallest_child(heap, index);
+	if (min == index)
+		return ;
+	swap_request(&heap->data[index], &heap->data[min]);
+	shift_down(heap, min);
 }
 
-void    heap_pop(t_heap *heap)
+void	heap_pop(t_heap *heap)
 {
-    heap->data[0] = heap->data[-1];
-    size--;
-    shift_down(heap, 0);
+	heap->data[0] = heap->data[-1];
+	size--;
+	shift_down(heap, 0);
 }
-    
+
+void	shift_up(t_heap *heap, int index)
+{
+	int	parent;
+
+	if (index == 0)
+		return ;
+	parent = (i - 1) / 2;
+	if (!higher_priority(heap->data[i], heap->data[parent]))
+		return ;
+	swap_request(&heap->data[i], &heap->data[parent]);
+	shift_up(heap, parent);
+}
+
+int	heap_push(t_heap *heap, t_request request)
+{
+	if (heap->size == heap->capacity)
+		return (0);
+	heap->data[heap->size] = request;
+	heap->size++;
+	shift_up(heap, heap->size - 1)
+	return (1);
+}
--- heap_utils.c
+++ heap_utils.c
@@ -31,3 +31,26 @@ int smallest_child(t_heap *heap, int index)
 			min = right;
 	return (min);
 }
+
+void	sift_up(t_heap *heap, int i)
+{
+	int	parent;
+
+	if (i == 0)
+		return ;
+	parent = (i - 1) / 2;
+	if (!request_is_before(heap->data[i], heap->data[parent]))
+		return ;
+	swap_request(&heap->data[i], &heap->data[parent]);
+	sift_up(heap, parent);
+}
+
+int	heap_push(t_heap *heap, t_request req)
+{
+	if (heap->size == heap->capacity)
+		return (0);
+	heap->data[heap->size] = req;
+	heap->size++;
+	sift_up(heap, heap->size - 1);
+	return (1);
+}
