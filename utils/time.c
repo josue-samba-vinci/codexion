c9b64f8b712339425b54e260293b9d250c031f77
Josue Samba <josuesamba99@gmail.com>
2026-09-07 17:40:26 +0200

    converting the long received with get_time_ms into sec and nsec

--- utils/time.c
+++ utils/time.c
@@ -32,3 +32,18 @@ void	precise_sleep(long duration_ms, t_config *config)
 		usleep(100);
 	}
 }
+
+void	long_to_timespec(long ms, struct timespec *ts)
+{
+	ts->tv_sec = ms / 1000;
+	ts->tv_nsec = ms % 1000 * 1000000;
+	return ;
+}
+int main()
+{
+	struct timespec ts;
+
+	long ms = get_time_ms();
+	long_to_timespec(ms, &ts);
+	printf("tv_sec = %ld, tv_nsec = %ld", ts.tv_sec, ts.tv_nsec);
+}
