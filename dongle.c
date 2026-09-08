df1d7d4a7e5cdda9e8f9c771a180c6bc66fe3bbb
Josue Samba <josuesamba99@gmail.com>
2026-09-08 17:31:31 +0200

    norm

--- dongle.c
+++ dongle.c
@@ -14,33 +14,24 @@
 
 int	take_one_dongle(t_coder *coder, t_dongle *dongle)
 {
+	t_request		request;
+
 	pthread_mutex_lock(&dongle->mutex);
-	struct timespec ts;
-	t_request request;
 	request.coder_id = coder->id;
 	request.key = choose_type_ticket(coder, dongle);
 	heap_push(&dongle->waiters, request);
-	while(!available_to_take(coder, dongle))
+	if (not_available_path(coder, dongle))
 	{
-		if (sim_is_over(coder->config))
-		{
-			pthread_mutex_unlock(&dongle->mutex);
-			return (0);
-		}
-		if (dongle->available == 0)
-			pthread_cond_wait(&dongle->cond, &dongle->mutex);
-		else
-			pthread_cond_timedwait(
-				&dongle->cond, &dongle->mutex, (dongle->free_at, &ts));
+		heap_pop(&dongle->waiters);
+		dongle->available = 0;
+		pthread_mutex_unlock(&dongle->mutex);
+		log_action(coder, "has taken a dongle");
+		return (1);
 	}
-	heap_pop(&dongle->waiters);
-	dongle->available = 0;
-	pthread_mutex_unlock(&dongle->mutex);
-	log_action(coder, "has taken a dongle");
-	return (1);
+	return (0);
 }
 
-void	take_dongles(t_coder *coder)
+int	take_dongles(t_coder *coder)
 {
 	if (!take_one_dongle(coder, coder->first_dongle))
 		return (0);
@@ -64,9 +55,9 @@ int	available_to_take(t_coder *coder, t_dongle *dongle)
 void	release_one_dongle(t_coder *coder, t_dongle *dongle)
 {
 	pthread_mutex_lock(&dongle-> mutex);
-	dongle->available = 1; //nobody is having it because i just releasedf it
-	dongle->free_at = get_time_ms() + coder->config->dongle_cooldown;// setting the time it will be available
-	pthread_cond_broadcast(&dongle->cond);//wakes up everyone (signal would wake up only one person BAD)
+	dongle->available = 1;
+	dongle->free_at = get_time_ms() + coder->config->dongle_cooldown;
+	pthread_cond_broadcast(&dongle->cond);
 	pthread_mutex_unlock(&dongle-> mutex);
 }
 
